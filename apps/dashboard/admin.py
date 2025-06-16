f57f7bc1b25056c5f83ccc9de8fc5bc3ca0628bb
HARSH17m <harshmistry137@gmail.com>
2025-06-16 14:18:39 +0530

    SUPERUSER

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['uid','name','email','is_active']
+    list_filter=['is_active']
+    list_editable=['name']
+    search_fields=['name','email']
+    
--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,8 +1,21 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 class Post(models.Model):
     image = models.ImageField(upload_to='post_images/')
     title = models.TextField(max_length=255,blank=True,null=True)
     context = models.TextField(max_length=255,blank=True,null=True)
     is_active = models.BooleanField(default=True)
+
+class BaseClass(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4,primary_key=True,null=False,blank=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+class User(BaseClass):
+    name=models.CharField(max_length=100,blank=False,null=False)
+    email=models.CharField(max_length=100,blank=False,null=False)
+    password=models.CharField(max_length=100,blank=False,null=False)
+    is_active=models.BooleanField(default=False)
